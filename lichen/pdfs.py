71af81f6cd9e197c204f1fb35342f830b12d0a19
Matthew Bellis <matthew.bellis@gmail.com>
2012-04-06 17:55:36 -0500

    B-mixing code is reasonably working and generates some events. No smearing yet.

--- lichen/pdfs.py
+++ lichen/pdfs.py
@@ -43,10 +43,24 @@ def red_chi_sq(func, x, y, dy, params):
 def pdf_bmixing(deltat,pars):
     
     gamma = pars[0]
-    p = pars[1]
-    q = pars[2]
-    deltaM = pars[3]
+    p_over_q = pars[1]
+    deltaM = pars[2]
+    deltaG = pars[3]
+    q1 = pars[4]
+    q2 = pars[5]
+
+    qq = q1*q2
+
+    A =  (1/2.0)*(1+qq)*(p_over_q**(2*q1)) + (1/2.0)*(1-qq) # coefficient of cosh term
+    B = -(1/2.0)*(1+qq)*(p_over_q**(2*q1)) + (1/2.0)*(1-qq) # coefficient of cos  term
+    C = 0.0 # coefficient of sinh term
+    D = 0.0 # coefficient of sin  term
+
+    #print "%d %f %f %f %f" % (qq,A,B,C,D)
     
-    N = (1.0/4.0)*np.exp(-gamma*np.abs(deltat))*(p/q)*(1-np.cos(deltaM*deltat))
+    N = (1.0/4.0)*np.exp(-gamma*np.abs(deltat))*(
+            A*np.cosh(deltaG*deltat/2)-B*np.cos(deltaM*deltat) +
+            C*np.sinh(deltaG*deltat/2)-D*np.sin(deltaM*deltat) )
+
     return N
 
