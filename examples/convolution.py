a9e040c0605776c48e1d363411ee41a4419b5c26
Matthew Bellis <matthew.bellis@gmail.com>
2012-04-09 15:40:33 -0500

    Convolution example getting there, but I don't know if it's right.

--- examples/convolution.py
+++ examples/convolution.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# Adapted from the Glowing python example.
+# http://glowingpython.blogspot.com/2012/02/convolution-with-numpy.html
+
 ################################################################################
 # Make a histogram and plot it on a figure (TCanvas).
 ################################################################################
@@ -15,6 +18,25 @@ import matplotlib.mlab as mlab
 import scipy.stats as stats
 import scipy.signal as signal
 
+def smear_with_gaussian_convolution(x,y,mean,sigma):
+
+    npts = len(x)
+
+    convolving_term = stats.norm(mean,sigma)
+
+    convolving_pts = convolving_term.pdf(x)
+
+    convolved_function = signal.convolve(y/y.sum(),convolving_pts)
+    print convolved_function
+
+    znpts = len(convolved_function)
+    begin = znpts/2 - npts/2
+    end = znpts/2 + npts/2
+
+    print "%d %d %d %d" % (npts,znpts,begin,end)
+
+    return convolved_function[begin:end],convolving_pts
+
 ################################################################################
 # main
 ################################################################################
@@ -24,7 +46,7 @@ def main():
     # Make a figure on which to plot stuff.
     # This would be the same as making a TCanvas object.
     ############################################################################
-    fig1 = plt.figure(figsize=(8,6),dpi=100,facecolor='w',edgecolor='k')
+    fig1 = plt.figure(figsize=(12,4),dpi=100,facecolor='w',edgecolor='k')
 
     ############################################################################
     # Now divide of the figure as if we were making a bunch of TPad objects.
@@ -35,32 +57,53 @@ def main():
     #               Z=which plot to plot based on the first being '1'.
     # So '111' is just one plot on the main figure.
     ############################################################################
-    subplot = fig1.add_subplot(1,1,1)
+    #subplot = fig1.add_subplot(1,1,1)
+
+    lo = -3
+    hi =  3
+    npts = 1000
 
     ############################################################################
     # Generate values drawn from a normal (Gaussian) distribution.
     ############################################################################
-    mean = 1.0
-    sigma = 0.2
+    mean = 0.0
+    sigma = 0.5
     rv = stats.norm(mean,sigma)
-    x = np.linspace(-np.minimum(rv.dist.b, 5), np.minimum(rv.dist.b, 5),100)
-    print x
-    h = plt.plot(x, rv.pdf(x))
+    x = np.linspace(lo,hi,npts)
+    #print x
+    gpts = rv.pdf(x)
+    fig1.add_subplot(1,3,2)
+    plt.plot(x,gpts,color='k')
 
-    subplot.set_xlim(-10,10)
+    ############################################################################
+    # Generate values drawn from a negative exponential
+    ############################################################################
+    tau = 1.0
+    x_exp = np.linspace(0,5,npts)
+    exp_pts = np.exp(x_exp*(-tau))
+    fig1.add_subplot(1,3,3)
+    plt.plot(x_exp,exp_pts,color='k')
 
     ############################################################################
     # Try the convolution
     ############################################################################
-    conv_mean = 0.0
-    conv_sigma = 1.2
-    conv_rv = stats.norm(conv_mean,conv_sigma)
-    y = np.linspace(-np.minimum(conv_rv.dist.b, 5), np.minimum(conv_rv.dist.b, 5),100)
-    print y
-    h = plt.plot(x, conv_rv.pdf(y))
+    conv_means = [0.0,0.0,-1.0]
+    conv_sigmas = [0.1,0.5,0.5]
+    colors = ['r','g','b']
+    for cm,cs,color in zip(conv_means,conv_sigmas,colors):
+        z,convpts = smear_with_gaussian_convolution(x,gpts,cm,cs)
+        fig1.add_subplot(1,3,1)
+        plt.plot(x,convpts)
 
-    z = signal.convolve(x,y)
-    print y
+        fig1.add_subplot(1,3,2)
+        plt.plot(x,z,color=color)
+
+        # Exponential
+        z,convpts = smear_with_gaussian_convolution(x,exp_pts,cm,cs)
+
+        fig1.add_subplot(1,3,3)
+        plt.plot(x_exp,z,color=color)
+        #plt.set_xlim(0,5)
 
     # Need this command to display the figure.
     plt.show()
