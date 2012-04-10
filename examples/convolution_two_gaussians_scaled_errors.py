cb89ced7827a505fc720a4dfc3d186dca0355b52
Matthew Bellis <matthew.bellis@gmail.com>
2012-04-09 22:34:40 -0500

    Added two more conv functions.

--- examples/convolution_two_gaussians_scaled_errors.py
+++ examples/convolution_two_gaussians_scaled_errors.py
@@ -21,28 +21,43 @@ import scipy.signal as signal
 ################################################################################
 # Convolving function.
 ################################################################################
-def smear_with_gaussian_convolution(x,y,yerr,means,sigmas):
+def smear_with_gaussian_convolution(x,y,xerr,means,sigmas):
 
     npts = len(x)
 
-    convolving_term = stats.norm(means[0],sigmas[0])
-    convolving_pts = convolving_term.pdf(x)
+    #convolving_term = stats.norm(means[0],sigmas[0])
+    convolving_pts = np.array([])
+    for i in range(0,npts):
+        convolving_term = stats.norm(means[0],sigmas[0]*xerr[i])
+        convolving_pts = np.append(convolving_pts,convolving_term.pdf(x[i]))
 
+        convolving_term = stats.norm(means[1],sigmas[1]*xerr[i])
+        convolving_pts[i] += convolving_term.pdf(x[i])
+
+    '''
     # Try adding another Gaussian to the.
     convolving_term_2 = stats.norm(means[1],sigmas[1])
 
     for i,pt in enumerate(convolving_pts):
         convolving_pts[i] += convolving_term_2.pdf(x[i])
+    '''
 
     convolving_pts /= 2.0
 
+    print len(x)
+    print len(y)
+    print len(xerr)
+
     convolved_function = signal.convolve(y/y.sum(),convolving_pts)
+
+    print len(convolved_function)
 
     # Have to carve out the middle of the curve, because
     # the returned array has too many points in it. 
+    print npts/2.0
     znpts = len(convolved_function)
-    begin = znpts/2 - npts/2
-    end = znpts/2 + npts/2
+    begin = int(znpts/2.0 - npts/2.0)
+    end = int(znpts/2.0 + npts/2.0)
 
     print "%d %d %d %d" % (npts,znpts,begin,end)
 
@@ -87,7 +102,7 @@ def main():
     gauss_err_func = stats.norm(0.0,1.0)
     gauss_err_pts = gauss_err_func.pdf(x)
 
-    fig1.add_subplot(1,3,2)
+    fig1.add_subplot(1,4,2)
     plt.plot(x,gpts,color='k')
     norm_norm = gpts.sum()*(x[3]-x[2])
     print norm_norm
@@ -101,27 +116,32 @@ def main():
     exp_norm = exp_pts.sum()*(x_exp[3]-x_exp[2])
     print exp_norm
     exp_pts /= exp_norm
-    fig1.add_subplot(1,3,3)
+    fig1.add_subplot(1,4,3)
     plt.plot(x_exp,exp_pts,color='k')
 
     ############################################################################
     # Try the convolution
     ############################################################################
     conv_means = [0.0,2.0]
-    conv_sigmas = [1.0,5.0]
+    conv_sigmas = [-0.1,5.0]
     
     z,convpts = smear_with_gaussian_convolution(x,gpts,gauss_err_pts,conv_means,conv_sigmas)
-    fig1.add_subplot(1,3,1)
+    fig1.add_subplot(1,4,1)
+    print len(x)
+    print len(convpts)
     plt.plot(x,convpts,color='r')
 
-    fig1.add_subplot(1,3,2)
+    fig1.add_subplot(1,4,2)
     plt.plot(x,z,color='r')
 
     # Exponential
     z,convpts = smear_with_gaussian_convolution(x,exp_pts,gauss_err_pts,conv_means,conv_sigmas)
 
-    fig1.add_subplot(1,3,3)
+    fig1.add_subplot(1,4,3)
     plt.plot(x_exp,z,color='r')
+
+    fig1.add_subplot(1,4,4)
+    plt.plot(x,gauss_err_pts,color='k')
 
     # Need this command to display the figure.
     plt.show()
