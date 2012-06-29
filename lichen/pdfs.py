ba64e61a58325480c1e69efca73ed0c3480a7259
Matthew Bellis <matthew.bellis@gmail.com>
2012-06-29 01:56:08 -0500

    Added pdfs to work with pyminuit.

--- lichen/pdfs.py
+++ lichen/pdfs.py
@@ -1,5 +1,69 @@
 import numpy as np
 import scipy as sp
+import scipy.stats as stats
+import scipy.integrate as integrate
+
+################################################################################
+# Exponential 
+# The slope is interpreted a negative
+################################################################################
+def exp(x,slope,xlo,xhi,num_int_points=1000):
+
+    exp_func = stats.expon(loc=0.0,scale=slope)
+
+    xnorm = np.linspace(xlo,xhi,num_int_points)
+    ynorm = exp_func.pdf(xnorm)
+    normalization = integrate.simps(ynorm,x=xnorm)
+    #normalization = 1.0
+    #print "pdfs normalization: ",normalization
+    
+    y = exp_func.pdf(x)/normalization
+
+    return y
+
+
+
+################################################################################
+# Gaussian
+################################################################################
+def gauss(x,mean,sigma,xlo,xhi,num_int_points=1000):
+
+    gauss_func = stats.norm(loc=mean,scale=sigma)
+
+    xnorm = np.linspace(xlo,xhi,num_int_points)
+    ynorm = gauss_func.pdf(xnorm)
+    normalization = integrate.simps(ynorm,x=xnorm)
+    #normalization = 1.0
+    #print "pdfs normalization: ",normalization
+    
+    y = gauss_func.pdf(x)/normalization
+
+    return y
+
+
+################################################################################
+# Polynomial
+################################################################################
+def poly(x,constants,xlo,xhi,num_int_points=1000):
+
+    npts = len(x)
+
+    poly = np.ones(npts)
+
+    xnorm = np.linspace(xlo,xhi,num_int_points)
+    ynorm = np.ones(num_int_points)
+
+    for i,c in enumerate(constants):
+        poly += c*np.pow(x,(i+1))
+        ynorm += c*np.pow(xnorm,(i+1))
+
+    normalization = integrate.simps(ynorm,x=xnorm)
+    #print "pdfs normalization: ",normalization
+    
+    return poly/normalization
+
+
+
 
 ################################################################################
 # Linear function
